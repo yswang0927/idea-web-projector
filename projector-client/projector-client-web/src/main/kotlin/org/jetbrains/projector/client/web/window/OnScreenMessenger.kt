20d8c7845e096440694c817be35e013f18d03c4a
yswang0927 <wangys0927@gmail.com>
2026-04-01 12:37:22 +0800

    增加refreshUrl和window.reconnectProjector.

--- projector-client/projector-client-web/src/main/kotlin/org/jetbrains/projector/client/web/window/OnScreenMessenger.kt
+++ projector-client/projector-client-web/src/main/kotlin/org/jetbrains/projector/client/web/window/OnScreenMessenger.kt
@@ -1,26 +1,3 @@
-/*
- * MIT License
- *
- * Copyright (c) 2019-2023 JetBrains s.r.o.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
 package org.jetbrains.projector.client.web.window
 
 import kotlinx.browser.document
@@ -31,8 +8,7 @@ import org.jetbrains.projector.client.web.state.LafListener
 import org.jetbrains.projector.client.web.state.ProjectorUI
 import org.jetbrains.projector.common.protocol.data.CommonRectangle
 import org.jetbrains.projector.util.logging.Logger
-import org.w3c.dom.HTMLDivElement
-import org.w3c.dom.HTMLParagraphElement
+import org.w3c.dom.*
 
 object OnScreenMessenger : LafListener {
 
@@ -64,7 +40,27 @@ object OnScreenMessenger : LafListener {
 
   private val reload = (document.createElement("div") as HTMLDivElement).apply {
     //innerHTML = "<p>If you wish, you can try to <a onclick='location.reload();' href=''>reconnect</a>.</p>"
-    innerHTML = "<p>如果您愿意，您可以尝试 <a onclick='location.reload();' href=''>重新连接</a>.</p>"
+    //innerHTML = "<p>如果您愿意，您可以尝试 <a onclick='window.reconnectProjector();' href='javascript:void(0)'>重新连接</a>.</p>"
+    
+    val p = document.createElement("p") as HTMLParagraphElement
+    p.textContent = "如果您愿意，您可以尝试 "
+
+    val a = document.createElement("a") as HTMLAnchorElement
+    a.textContent = "重新连接"
+    a.href = "javascript:void(0)"
+    
+    a.onclick = { event ->
+      event.preventDefault() 
+      val dynamicWindow = window.asDynamic()
+      if (dynamicWindow.reconnectProjector != undefined) {
+        dynamicWindow.reconnectProjector()
+      } else {
+        window.location.reload()
+      }
+    }
+
+    p.appendChild(a)
+    appendChild(p)
 
     div.appendChild(this)
   }
